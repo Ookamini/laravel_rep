397548d2d5593541753f5183d5a33f65d082e52f
User <test@yandex.ru>
2016-06-11 16:37:11 +0900

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 DOSVIDULI</div>
+                <div class="title">Laravel 5 DOSVIDULIa</div>
             </div>
         </div>
     </body>
